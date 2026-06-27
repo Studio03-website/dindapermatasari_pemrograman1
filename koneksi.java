6d8631d119d8d2902b1c77d51169148d4d1d83ed
DINDA PERMATASARI <dins.studio03@gmail.com>
2026-06-27 14:43:12 +0700

    Add files via upload

---
+++ koneksi.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+
+        if (koneksi == null) {
+
+            try {
+
+                String url =
+                    "jdbc:mysql://localhost:3307/rental_mobil";
+
+                String user = "root";
+                String pass = "";
+
+                DriverManager.registerDriver(
+                    new com.mysql.cj.jdbc.Driver());
+
+                koneksi =
+                    DriverManager.getConnection(url, user, pass);
+
+                System.out.println("Koneksi Berhasil");
+
+            } catch (SQLException e) {
+
+                System.out.println("Koneksi Gagal");
+                System.out.println(e.getMessage());
+
+            }
+        }
+
+        return koneksi;
+    }
+}
